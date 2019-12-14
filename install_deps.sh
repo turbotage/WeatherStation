d1ab18f809089ffc193e01bef85c63115b579a36
turbotage <viktor.vigrennaslund@live.se>
2019-12-14 10:58:54 +0100

    worked on shell cmds

--- install_deps.sh
+++ install_deps.sh
@@ -1,20 +1,22 @@
 rm -rf FetchAndServe/fetcher/node_modules
 rm -rf FetchAndServe/server/node_modules
 cd FetchAndServe/fetcher/
+npm install forever
 npm install mysql
 npm install minimist
 npm install random
 npm install date-and-time
 npm install serialport
+chmod +x run_fetcher_test.sh
+chmod +x run_fetcher.sh
 cd ..
 cd server/
+npm install forever
 npm install express
 npm install mysql
 npm install minimist
 npm install socket.io
-cd ..
-cd ..
-chmod +x run_fetcher_test.sh
-chmod +x run_fetcher.sh
 chmod +x run_server_test.sh
 chmod +x run_server.sh
+cd ..
+cd ..
